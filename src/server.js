fa318cbb1c7303ecced464b55ab5ec88be278b21
Kavyagupta070707 <kavyagupta0105@gmail.com>
2026-01-12 23:29:33 +0530

    sream cors

--- src/server.js
+++ src/server.js
@@ -1,21 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
-import path from "path";
-import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = ENV.PORT
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 let isConnected = false;
 app.use(async (req, res, next) => {
@@ -26,27 +19,31 @@ app.use(async (req, res, next) => {
   next();
 });
 
-app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173', ENV.CLIENT_URL],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      ENV.CLIENT_URL, 
+    ],
     credentials: true,
-}));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.options("*", cors());
+
+app.use(express.json());
+
 app.use(clerkMiddleware());
-app.use("/api/inngest", serve({ client: inngest, functions })) // deployment ke baad inngest me Apps me frontend ka URL daalna h
-app.use("/api/chat", chatRoutes)
-app.use("/api/sessions", sessionRoutes)
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello, world!" });
-})
+  res.json({ message: "Hello, world!" });
+});
 
 
-
-
-
-
-
-
-// For Vercel serverless
 export default app;
