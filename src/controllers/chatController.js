87d54915ab135e64ba798e8d00fdb9b03fbb003e
Kavyagupta070707 <kavyagupta0105@gmail.com>
2026-01-13 16:24:37 +0530

    token

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -2,14 +2,22 @@ import { streamClient } from "../lib/stream.js";
 
 export async function getStreamtoken(req,res){
     try {
+         if (!req.user || !req.user.clerkId) {
+            return res.status(401).json({ error: "Unauthorized - User not authenticated" });
+        }
+
         const token = streamClient.createToken(req.user.clerkId)
         res.status(200).json({
-            toke,
+            token,
             userId: req.user.clerkId,
             username: req.user.name,
             userImage: req.user.image
         })
     } catch (error) {
-        
+         console.error("Error generating Stream token:", error);
+        res.status(500).json({ 
+            error: "Failed to generate Stream token",
+            message: error.message 
+        });
     }
 }
