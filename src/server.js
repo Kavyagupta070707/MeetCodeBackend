c63dbc8e54f6723a22fb5898b3c8741069501ea5
Kavyagupta070707 <kavyagupta0105@gmail.com>
2026-01-13 16:05:29 +0530

    origin

--- src/server.js
+++ src/server.js
@@ -21,10 +21,7 @@ app.use(async (req, res, next) => {
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      ENV.CLIENT_URL, 
-    ],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
